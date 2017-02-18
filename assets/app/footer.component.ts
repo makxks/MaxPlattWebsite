086d310309dcaae5decab6be3340b87ef9a20008
makxks <makxks@users.noreply.github.com>
2017-02-18 09:40:15 +0800

    Add name and contact to footer

--- assets/app/footer.component.ts
+++ assets/app/footer.component.ts
@@ -4,6 +4,8 @@ import { Component } from '@angular/core';
 	selector: 'app-footer',
 	template: `
 		<footer>
+			<p>Max Platt</p>
+			<p>Contact: <a href="maxplatt@outlook.com>maxplatt@outlook.com</a>
 		</footer>
 	`,
   styleUrls: ['./footer.component.css']
