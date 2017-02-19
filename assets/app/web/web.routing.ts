cb1e69b70d8165f8a3cfa04d11ca031bf1d30315
makxks <makxks@users.noreply.github.com>
2017-02-19 13:27:06 +0800

    add commented routes to use later

--- assets/app/web/web.routing.ts
+++ assets/app/web/web.routing.ts
@@ -1,8 +1,16 @@
 import { Routes, RouterModule } from '@angular/router';
 
 import { WebHomeComponent } from './webhome.component';
+// import { RockLimeComponent } from './rocklime.component';
+// import { MandarinLaneComponent } from './mandarinlane.component';
+// import { PatsStatsComponent } from './patsstats.component';
+// import { ChatAppComponent } from './chatapp.component';
 
 export const WEB_ROUTES: Routes = [
 	{ path: '' , redirectTo: 'webhome', pathMatch: 'full' },
 	{ path: 'webhome', component: WebHomeComponent }
+	//{ path: 'rocklime', component: RockLimeComponent },
+	//{ path: 'mandarinlane, component: MandarinLaneComponent },
+	//{ path: 'patsstats', component: PatsStatsComponent },
+	//{ path: 'chatapp', component: ChatAppComponent }
 ]
