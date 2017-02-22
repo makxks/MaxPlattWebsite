57fcb91f72cb8a9ae27edd16bc9da664aef61614
makxks <makxks@users.noreply.github.com>
2017-02-22 15:00:17 +0800

    add rocklime route

--- assets/app/web/web.routing.ts
+++ assets/app/web/web.routing.ts
@@ -1,15 +1,15 @@
 import { Routes, RouterModule } from '@angular/router';
 
 import { WebHomeComponent } from './webhome.component';
-// import { RockLimeComponent } from './rocklime/rocklime.component';
+import { RockLimeComponent } from './rocklime/rocklime.component';
 // import { MandarinLaneComponent } from './jasonwebsite/mandarinlane.component';
 // import { PatsStatsComponent } from './patsstats/patsstats.component';
 // import { ChatAppComponent } from './chapp/chatapp.component';
 
 export const WEB_ROUTES: Routes = [
 	{ path: '' , redirectTo: 'webhome', pathMatch: 'full' },
-	{ path: 'webhome', component: WebHomeComponent }
-	//{ path: 'rocklime', component: RockLimeComponent },
+	{ path: 'webhome', component: WebHomeComponent },
+	{ path: 'rocklime', component: RockLimeComponent }
 	//{ path: 'mandarinlane, component: MandarinLaneComponent },
 	//{ path: 'patsstats', component: PatsStatsComponent },
 	//{ path: 'chatapp', component: ChatAppComponent }
