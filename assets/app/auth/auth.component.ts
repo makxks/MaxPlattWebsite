da4c3abb5c4ac5cbc6f266b974fcc1413ac409f0
Maximilian G. Platt <maxplatt7@gmail.com>
2017-08-03 10:34:08 +0800

    removals

--- assets/app/auth/auth.component.ts
+++ assets/app/auth/auth.component.ts
@@ -8,7 +8,7 @@ import { AuthService } from './auth.service';
 		<div class="authComponentsContainer">
 			<header>
 				<nav class="col-lg-8 col-md-8 col-sm-8 col-xs-8 col-lg-offset-2 col-md-offset-2 col-sm-offset-2 col-xs-offset-2">
-					<ul class="nav nav-tabs">
+					<ul class="nav nav-tabs nav-justified">
 						<li routerLinkActive="active"><a [routerLink]="['signup']">Signup</a></li>
 						<li routerLinkActive="active"><a [routerLink]="['signin']" *ngIf="!isAuth()">Signin</a></li>
 						<li><a (click)="onLogout()" *ngIf="isAuth()" style="cursor: pointer;">Logout</a></li>
--- assets/app/logo.component.ts
+++ assets/app/logo.component.ts
@@ -4,15 +4,10 @@ import { Component } from '@angular/core';
 	selector: 'app-logo',
 	template: `
 	<div class="row">
-		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 mask">
-			<img id="topImage" src="/images/bg2.png" alt="background image (from survivor)">
-		</div>
 		<div id="logoPanel">
-	  	<div class="well">
-				<h1 routerLinkActive="active"><a [routerLink]="['/home']">MAX PLATT</a></h1>
-				<hr>
-				<h3 id="subheading">Web and Game</h3>
-			</div>
+			<h1 routerLinkActive="active"><a [routerLink]="['/home']">MAX PLATT</a></h1>
+			<hr>
+			<h3 id="subheading">Web and Game</h3>
 		</div>
   </div>
   `,
